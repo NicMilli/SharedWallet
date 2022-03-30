7000bfac52fc6edd145cb41c01c55eb924dd8d32
NicMilli <91165462+NicMilli@users.noreply.github.com>
2022-03-30 16:45:11 -0400

    Updates

---
+++ precache-manifest.19c9b1727a1b84de64321baf4fb51961.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c0be32b60235f9c8d155779a53980ed",
+    "url": "/SharedWallet/index.html"
+  },
+  {
+    "revision": "41ef63714e0df14021bf",
+    "url": "/SharedWallet/static/css/main.edd24312.chunk.css"
+  },
+  {
+    "revision": "d8234de87691829b152e",
+    "url": "/SharedWallet/static/js/2.be8375ad.chunk.js"
+  },
+  {
+    "revision": "41ef63714e0df14021bf",
+    "url": "/SharedWallet/static/js/main.caa6cccf.chunk.js"
+  },
+  {
+    "revision": "e1d0d4d54190fa02e1c1",
+    "url": "/SharedWallet/static/js/runtime-main.9c159c29.js"
+  },
+  {
+    "revision": "b8d3a56101966b2394dfad32bbb3d5df",
+    "url": "/SharedWallet/static/media/mylogo.b8d3a561.png"
+  }
+]);
